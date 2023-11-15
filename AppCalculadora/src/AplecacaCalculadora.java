22dccbd229c9118178eb00e3bf12ea2d1240ff8b
JonasFilho91 <jonas16051991@gmail.com>
2023-11-15 18:24:28 -0300

    Uploud inicial

---
+++ AppCalculadora/src/AplecacaCalculadora.java
@@ -0,0 +1,9 @@
+
+public class AplecacaCalculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("meu primeiro programa pelo git");
+	}
+
+}
